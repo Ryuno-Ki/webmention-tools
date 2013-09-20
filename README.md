87f0ffa079b69a322c6b624c9b3e233c75c6128a
Panayotis Vryonis <vrypan@gmail.com>
2013-09-20 16:13:55 +0300

    Minor code improvements

--- README.md
+++ README.md
@@ -3,30 +3,51 @@ webmention-tools
 
 Some simple tools in python to deal with webmentions.
 
-Currently, webmentiontools.send implements WebmentionSend 
+Currently: 
+
+- webmentiontools.send implements WebmentionSend 
 that sends webmentions.
+- webmentiontools.urlinfo implements UrlInfo() that
+will rerurn usefull information about a web page, like 
+title, the existance of an "in-reply-to" link,
+the author name, the author image, etc.
 
 There is also the corresponting command line tool, 
-webmention-send.py (which is also a simple example on how 
+webmention-tools (which is also a simple example on how 
 to use the library.
 
 
 Installation
 ============
 
-pip install webmention-tools
+pip install webmentiontools
 
 Usage: 
 ========
 
 Command line:
 
-    webmention-send.py source target
+    webmention-tools send `source` `target`
+    webmention-tools urlinfo `url`
 
 or
 
 Python code:
 
     from webmentiontools.send import WebmentionSend
+    source = 'URL of page sending the webmention'
+    target = 'URL of page to receive the webmention'
     mention = WebmentionSend(source, target)
     mention.send()
+
+    from webmentiontools.urlinfo import UrlInfo
+    url = 'a link to a web page'
+    i = UrlInfo(url)
+    if i.error:
+        print 'There was an error getting %s' % url
+    else:
+        print 'in-reply-to link: %s' % i.inReplyTo()
+        print 'publication date: %s' % i.pubDate()
+        print 'page title: %s' % i.title()
+        print 'image link: %s' % i.image()
+
--- webmentiontools/urlinfo.py
+++ webmentiontools/urlinfo.py
@@ -13,6 +13,8 @@ class UrlInfo():
 
     def fetchHTML(self):
         self.soup = None
+        self.data = dict()
+        self.data['links_to'] = []
         r = requests.get(self.url)
         if r.status_code != 200:
             self.error = True
@@ -20,6 +22,8 @@ class UrlInfo():
         self.soup = BeautifulSoup(r.text)
 
     def inReplyTo(self):
+        if self.data.has_key('in_reply_to'):
+            return self.data['in_reply_to']
         # Identify first class="u-in-reply-to" or rel="in-reply-to" link
         ir2 = self.soup.find('a', attrs={'class':'u-in-reply-to'})
         if not ir2:
@@ -27,39 +31,56 @@ class UrlInfo():
             if not ir2:
                 return None
         ir2_link = ir2['href']
+        self.data['in_reply_to'] = ir2_link
         return ir2_link
 
     def pubDate(self):
         # Get the time of the reply, if possible
+        if self.data.has_key('pubDate'):
+            return self.data['pubDate']
+
         ir2_time = self.soup.find(True, attrs={'class':'dt-published'})
         if ir2_time  and ir2_time.has_attr('datetime') :
+            self.data['pubDate'] = ir2_time['datetime']
             return ir2_time['datetime']
         
     def title(self):
+        if self.data.has_key('title'):
+            return self.data['title']
         # Get the title
         title = self.soup.find('title').string
+        self.data['title'] = title
         return title
 
     def image(self):
+        if self.data.has_key('image'):
+            return self.data['image']
         author = self.soup.find(True, attrs={'class':'p-author'})
         if author:
             image = author.find('img')
             if image:
                 image_src = image['src']
+                self.data['image'] = image_src
                 return image_src
         hcard = self.soup.find(True, attrs={'class':'h-card'})
         if hcard:
             image = hcard.find('img', attrs={'class':'u-photo'})
             if image:
+                self.data['image'] = image['src']
                 return image['src']
 
+    def linksTo(self, url):
+        # Check if page links to a specific URL.
+        # please note that the test is done on the *exact* URL. If
+        # you want to ignore ?parameters, please remove them in advance
+        if url in self.data['links_to']:
+            return True
+        r = self.soup.find("a", attrs={'href': url})
+        if r:
+            self.data['links_to'].append(url)
+            return True
+        else:
+            return False
 
 if __name__ == '__main__':
-   url = 'enter a url to test here'
-   a = UrlInfo(url)
-   if not a.error:
-       print
-       print a.inReplyTo()
-       print a.pubDate()
-       print a.title()
-       print a.image()
+   pass
