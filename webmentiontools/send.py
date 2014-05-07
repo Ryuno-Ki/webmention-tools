Ryuno-Ki/webmention-tools

commit f29879a29617960f3a680cba015af2858d0cd167

discover webmention endpoints in <a> tags as well as <link>
background: converspace/webmention#32

webmentiontools/send.py
@@ -49,12 +49,14 @@ class WebmentionSend():
         # look in the content
         html = r.text
         soup = BeautifulSoup(html)
-        tag = soup.find('link', attrs={'rel': 'webmention'})
-        if not tag:
-            # backward compatibility with the webmention 0.1 spec
-            tag = soup.find('link', attrs={'rel': 'http://webmention.org/'})
+        tag = None
+        for name in 'link', 'a':
+            for rel in 'webmention', 'http://webmention.org/':
+                tag = soup.find(name, attrs={'rel': rel})
+                if tag:
+                    break
 
-        if tag and tag['href']:
+        if tag and tag.get('href'):
             # add the base scheme and host to relative endpoints
             self.receiver_endpoint = urlparse.urljoin(self.target_url, tag['href'])
         else:

commit 4c95e165c6b5664d4617a3cf716aa556b4e8d8e5

fix bug introduced by f29879a29617960f3a680cba015af2858d0cd167
break only breaks out of one loop :P

webmentiontools/send.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import itertools
 import urlparse
 import re
 import requests
@@ -50,11 +51,10 @@ class WebmentionSend():
         html = r.text
         soup = BeautifulSoup(html)
         tag = None
-        for name in 'link', 'a':
-            for rel in 'webmention', 'http://webmention.org/':
-                tag = soup.find(name, attrs={'rel': rel})
-                if tag:
-                    break
+        for name, rel in itertools.product(('link', 'a'), ('webmention', 'http://webmention.org/')):
+            tag = soup.find(name, attrs={'rel': rel})
+            if tag:
+                break
 
         if tag and tag.get('href'):
             # add the base scheme and host to relative endpoints