713d5f2167ef14ab62aa8adc35423eef84d458b5
André Jaenisch <andre.jaenisch@posteo.de>
2019-04-24 23:04:26 +0200

    Update maintainer information.

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from webmentiontools import __version__
 
 setup(version=__version__,
       name="webmentiontools",
-      author="Panayotis Vryonis",
-      author_email="vrypan@gmail.com",
+      author="André Jaenisch",
+      author_email="andre.jaenisch@posteo.de",
       description="Tools for webmention.org.",
       long_description=open('README.md').read(),
       packages=['webmentiontools'],
       install_requires=['beautifulsoup4', 'requests', 'docopt',],
       scripts=['bin/webmention-tools'],
-      url='https://github.com/vrypan/webmention-tools',
+      url='https://github.com/Ryuno-Ki/webmention-tools',
       license='LICENSE',
       include_package_data=True,
 )
