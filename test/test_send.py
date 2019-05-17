0c648197c031d8ec9989875e5c5914baabb5b375
André Jaenisch <andre.jaenisch@posteo.de>
2019-05-17 22:59:41 +0200

    test(send): moved endpoints into helper file
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- test/test_send.py
+++ test/test_send.py
@@ -5,76 +5,10 @@ import pytest
 import webmentiontools
 from webmentiontools.send import WebmentionSend
 
+from .endpoints import WEBMENTION_ROCKS_TESTS
+
 MOCK_SOURCE = 'http://example.com/'
 MOCK_TARGET = 'http://foo.bar/'
-
-WEBMENTION_ROCKS_TESTS = [{
-    "test": "https://webmention.rocks/test/1",
-    "endpoint": "https://webmention.rocks/test/1/webmention?head=true"
-}, {
-    "test": "https://webmention.rocks/test/2",
-    "endpoint": "https://webmention.rocks/test/2/webmention?head=true"
-}, {
-    "test": "https://webmention.rocks/test/3",
-    "endpoint": "https://webmention.rocks/test/3/webmention"
-}, {
-    "test": "https://webmention.rocks/test/4",
-    "endpoint": "https://webmention.rocks/test/4/webmention"
-}, {
-    "test": "https://webmention.rocks/test/5",
-    "endpoint": "https://webmention.rocks/test/5/webmention"
-}, {
-    "test": "https://webmention.rocks/test/6",
-    "endpoint": "https://webmention.rocks/test/6/webmention"
-}, {
-    "test": "https://webmention.rocks/test/7",
-    "endpoint": "https://webmention.rocks/test/7/webmention?head=true"
-}, {
-    "test": "https://webmention.rocks/test/8",
-    "endpoint": "https://webmention.rocks/test/8/webmention?head=true"
-}, {
-    "test": "https://webmention.rocks/test/9",
-    "endpoint": "https://webmention.rocks/test/9/webmention"
-}, {
-    "test": "https://webmention.rocks/test/10",
-    "endpoint": "https://webmention.rocks/test/10/webmention?head=true"
-}, {
-    "test": "https://webmention.rocks/test/11",
-    "endpoint": "https://webmention.rocks/test/11/webmention"
-}, {
-    "test": "https://webmention.rocks/test/12",
-    "endpoint": "https://webmention.rocks/test/12/webmention"
-}, {
-    "test": "https://webmention.rocks/test/13",
-    "endpoint": "https://webmention.rocks/test/13/webmention"
-}, {
-    "test": "https://webmention.rocks/test/14",
-    "endpoint": "https://webmention.rocks/test/14/webmention"
-}, {
-    "test": "https://webmention.rocks/test/15",
-    "endpoint": "https://webmention.rocks/test/15"
-}, {
-    "test": "https://webmention.rocks/test/16",
-    "endpoint": "https://webmention.rocks/test/16/webmention"
-}, {
-    "test": "https://webmention.rocks/test/17",
-    "endpoint": "https://webmention.rocks/test/17/webmention"
-}, {
-    "test": "https://webmention.rocks/test/18",
-    "endpoint": "https://webmention.rocks/test/18/webmention?head=true"
-}, {
-    "test": "https://webmention.rocks/test/19",
-    "endpoint": "https://webmention.rocks/test/19/webmention?head=true"
-}, {
-    "test": "https://webmention.rocks/test/20",
-    "endpoint": "https://webmention.rocks/test/20/webmention"
-}, {
-    "test": "https://webmention.rocks/test/21",
-    "endpoint": "https://webmention.rocks/test/21/webmention?query=yes"
-}, {
-    "test": "https://webmention.rocks/test/22",
-    "endpoint": "https://webmention.rocks/test/22/webmention"
-}]
 
 
 class WebmentionSendTestCase(unittest.TestCase):
